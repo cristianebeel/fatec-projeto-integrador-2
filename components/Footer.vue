ceb9112dd838046000fe27fa79f4a8e59dca1600
Belle <cristianebeel@live.com>
2021-03-25 18:09:54 -0300

    Rodapé arrumado

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <section id="footer">
-    <footer class="bg-pink-200 py-6">
-        <div class="flex justify-evenly">
+    <footer class="bg-pink-200 py-4 text-center">
+        <div class="flex justify-evenly content-start">
         <div>
             <h3>Serviços</h3>
             <ul>
@@ -13,28 +13,55 @@
         <div>
             <h3>Institucional</h3>
             <ul>
+            <li>Quem somos</li>
             <li>Trabalhe conosco</li>
-            <li>Fale Conosco</li>
             </ul>
         </div>
         <div>
             <h3>Formas de pagamento</h3>
             <ul class="flex justify-evenly">
-            <li>crédito</li>
-            <li>débito</li>
-            <li>boleto</li>
+            <li><img src="../assets/img/pay/mastercard.png" alt="Cartão Mastercard"></li>
+            <li><img src="../assets/img/pay/visa.png" alt="Cartão Visa"></li>
+            <li><img src="../assets/img/pay/elo.png" alt="Cartão Elo"></li>
+            <li><img src="../assets/img/pay/boleto.png" alt="Boleto"></li>
+            <li><img src="../assets/img/pay/pix.png" alt="Pix"></li>
+            <li><img src="../assets/img/pay/paypal.png" alt="PayPal"></li>
             </ul>
         </div>
         <div>
             <h3>Siga-nos</h3>
             <ul class="flex justify-evenly">
-            <li>face</li>
-            <li>insta</li>
-            <li>yt</li>
+            <li><a href="#" target="_blank"><img src="../assets/img/facebook.svg" alt="Facebook"></a></li>
+            <li><a href="#" target="_blank"><img src="../assets/img/instagram.svg" alt="Instagram"></a></li>
             </ul>
         </div>
         </div>
     </footer>
-    <p class="bg-pink-500 text-white p-3 text-center">&copy; Todos os direitos reservados</p>
+    <p class="flex justify-center text-white p-3 text-lg tracking-wide">Romapel Papelaria <span class="text-xl">&reg;</span> Todos os direitos reservados</p>
     </section>
 </template>
+
+<style scoped>
+h3, ul li{
+    padding: .3rem;
+}
+
+h3{
+    font-size: 1.4rem;
+    letter-spacing: .1rem;
+    padding-bottom: .5rem;
+}
+
+ul li{
+    @apply text-base;
+}
+
+p{
+    background-color: #ff3c77;
+}
+
+img{
+    height: 3.5rem;
+    width: auto;
+}
+</style>
