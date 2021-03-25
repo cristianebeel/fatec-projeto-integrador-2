91af632174bbfa0a07019f582a907a51e791e088
Belle <cristianebeel@live.com>
2021-03-25 13:29:12 -0300

    Arrumando imagens, navbar e carousel

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -4,38 +4,38 @@
             <div class="carousel-inner relative overflow-hidden w-full">
             <!--Slide 1-->
             <input class="carousel-open" type="radio" id="carousel-1" name="carousel" aria-hidden="true" hidden="" checked="checked" />
-            <div class="carousel-item absolute opacity-0" style="height:50vh;">
-                <div class="block h-full w-full bg-indigo-500 text-white text-5xl text-center">Slide 1</div>
+            <div class="carousel-item absolute opacity-0" style="height:55vh;">
+                <div class="car-banner-01 h-full w-full flex items-center justify-center">Materiais Escolares</div>
             </div>
-            <label for="carousel-3" class="prev control-1 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
-            <label for="carousel-2" class="next control-1 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
+            <label for="carousel-3" class="prev control-1 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-pink-400 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
+            <label for="carousel-2" class="next control-1 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-pink-400 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
 
             <!--Slide 2-->
             <input class="carousel-open" type="radio" id="carousel-2" name="carousel" aria-hidden="true" hidden="" />
-            <div class="carousel-item absolute opacity-0" style="height:50vh;">
-                <div class="block h-full w-full bg-orange-500 text-white text-5xl text-center">Slide 2</div>
+            <div class="carousel-item absolute opacity-0" style="height:55vh;">
+                <div class="car-banner-02 h-full w-full flex items-center justify-center">Suprimentos para Escritório</div>
             </div>
-            <label for="carousel-1" class="prev control-2 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
-            <label for="carousel-3" class="next control-2 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
+            <label for="carousel-1" class="prev control-2 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-pink-400 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
+            <label for="carousel-3" class="next control-2 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-pink-400 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
 
             <!--Slide 3-->
             <input class="carousel-open" type="radio" id="carousel-3" name="carousel" aria-hidden="true" hidden="" />
-            <div class="carousel-item absolute opacity-0" style="height:50vh;">
-                <div class="block h-full w-full bg-green-500 text-white text-5xl text-center">Slide 3</div>
+            <div class="carousel-item absolute opacity-0" style="height:55vh;">
+                <div class="car-banner-03 h-full w-full flex items-center justify-center">Arte & Pintura</div>
             </div>
-            <label for="carousel-2" class="prev control-3 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
-            <label for="carousel-1" class="next control-3 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
+            <label for="carousel-2" class="prev control-3 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-pink-400 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
+            <label for="carousel-1" class="next control-3 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-pink-400 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
 
             <!-- Add additional indicators for each slide-->
             <ol class="carousel-indicators">
                 <li class="inline-block mr-3">
-                <label for="carousel-1" class="carousel-bullet cursor-pointer block text-4xl text-white hover:text-blue-700">•</label>
+                <label for="carousel-1" class="carousel-bullet cursor-pointer block text-4xl text-gray-400">•</label>
                 </li>
                 <li class="inline-block mr-3">
-                <label for="carousel-2" class="carousel-bullet cursor-pointer block text-4xl text-white hover:text-blue-700">•</label>
+                <label for="carousel-2" class="carousel-bullet cursor-pointer block text-4xl text-gray-400">•</label>
                 </li>
                 <li class="inline-block mr-3">
-                <label for="carousel-3" class="carousel-bullet cursor-pointer block text-4xl text-white hover:text-blue-700">•</label>
+                <label for="carousel-3" class="carousel-bullet cursor-pointer block text-4xl text-gray-400">•</label>
                 </li>
             </ol>
             </div>
@@ -44,37 +44,63 @@
 </template>
 
 <style scoped>
-    .carousel-open:checked + .carousel-item {
-        position: static;
-        opacity: 100;
-    }
+.carousel-open:checked + .carousel-item {
+    position: static;
+    opacity: 100;
+}
 
-    .carousel-item {
-        -webkit-transition: opacity 0.6s ease-out;
-        transition: opacity 0.6s ease-out;
-    }
+.carousel-item {
+    -webkit-transition: opacity 0.6s ease-out;
+    transition: opacity 0.6s ease-out;
+}
 
-    #carousel-1:checked ~ .control-1,
-    #carousel-2:checked ~ .control-2,
-    #carousel-3:checked ~ .control-3 {
-        display: block;
-    }
+.carousel-item p{
+    @apply text-gray-800;
+}
 
-    .carousel-indicators {
-        list-style: none;
-        margin: 0;
-        padding: 0;
-        position: absolute;
-        bottom: 2%;
-        left: 0;
-        right: 0;
-        text-align: center;
-        z-index: 10;
-    }
+#carousel-1:checked ~ .control-1,
+#carousel-2:checked ~ .control-2,
+#carousel-3:checked ~ .control-3 {
+    display: block;
+}
 
-    #carousel-1:checked ~ .control-1 ~ .carousel-indicators li:nth-child(1) .carousel-bullet,
-    #carousel-2:checked ~ .control-2 ~ .carousel-indicators li:nth-child(2) .carousel-bullet,
-    #carousel-3:checked ~ .control-3 ~ .carousel-indicators li:nth-child(3) .carousel-bullet {
-        color: #2b6cb0; /*Set to match the Tailwind colour you want the active one to be */
-    }
+.carousel-indicators {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    position: absolute;
+    bottom: 2%;
+    left: 0;
+    right: 0;
+    text-align: center;
+    z-index: 10;
+}
+
+.carousel-indicators label:hover{
+    @apply text-pink-400;
+}
+
+#carousel-1:checked ~ .control-1 ~ .carousel-indicators li:nth-child(1) .carousel-bullet,
+#carousel-2:checked ~ .control-2 ~ .carousel-indicators li:nth-child(2) .carousel-bullet,
+#carousel-3:checked ~ .control-3 ~ .carousel-indicators li:nth-child(3) .carousel-bullet {
+    @apply text-pink-300;
+}
+
+.car-banner-01{
+    background-image: url(../assets/img/banner-01.jpg);
+}
+
+.car-banner-02{
+    background-image: url(../assets/img/banner-02.jpg);
+}
+
+.car-banner-03{
+    background-image: url(../assets/img/banner-03.jpg);
+}
+
+.car-banner-01, .car-banner-02, .car-banner-03{
+    background-size: 100%  auto;
+    background-position: center;
+    font-size: 4.5rem;
+}
 </style>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,16 +1,29 @@
 <template>
-    <section id="navbar">
-        <nav class="bg-pink-600 text-white p-5">
-            <div class="flex justify-between py-9">
-            <nuxt-link to="/">Logo</nuxt-link>
-            <div class="flex space-x-40">
-                <span>Escolar</span>
-                <span>Escritório</span>
-                <span>Arte e Papelaria</span>
-                <span>Contato</span>
-            </div>
-            <div>Carrinho</div>
+    <section id="navbar shadow-2xl">
+        <nav class="text-white text-2xl p-2">
+            <div class="flex justify-around items-center pr-10 py-0">
+                <nuxt-link to="/"><Logo /></nuxt-link>
+                <div class="flex space-x-40 py-2">
+                    <nuxt-link to="/" class="link">Escolar</nuxt-link>
+                    <nuxt-link to="/" class="link">Escritório</nuxt-link>
+                    <nuxt-link to="/" class="link">Arte e Papelaria</nuxt-link>
+                    <nuxt-link to="/" class="link">Contato</nuxt-link>
+                </div>
             </div>
         </nav>
     </section>
 </template>
+
+<style scoped>
+nav{
+    background-color: #ff3c77;
+
+}
+nav img{
+    width: 5rem;
+}
+
+.link:hover{
+    text-decoration: underline;
+}
+</style>
