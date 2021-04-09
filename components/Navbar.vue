7743e77b92ce9ec849c87237a3a1942a975f88b1
cristianebeel <cristianebeel@live.com>
2021-04-09 01:17:16 -0300

    Renomeando página

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -6,7 +6,7 @@
                 <div class="flex space-x-40 py-2">
                     <nuxt-link to="/" class="link">Escolar</nuxt-link>
                     <nuxt-link to="/escritorio" class="link">Escritório</nuxt-link>
-                    <nuxt-link to="/artes-e-pintura" class="link">Artes e Papelaria</nuxt-link>
+                    <nuxt-link to="/artes-e-pintura" class="link">Artes e Pintura</nuxt-link>
                     <nuxt-link to="/contato" class="link">Contato</nuxt-link>
                 </div>
             </div>
