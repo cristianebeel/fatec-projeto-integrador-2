9751c50eea20a8512a772842dff2b0e8398b53a4
Maria Silvia <msylviacb@gmail.com>
2021-04-27 22:30:35 -0300

    new

--- pages/Artes-e-Pintura.vue
+++ pages/Artes-e-Pintura.vue
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tailwind CSS Tutorial by  Tradydaddy</title>
+    <title>Tailwind CSS</title>
     <script src="https://use.fontawesome.com/b658ad5274.js"></script>
     <link rel="stylesheet" href="css/output.css">
 </head>
@@ -16,10 +16,10 @@
         <div class="relative ">
             <div class="sliderAx h-4/5">
                 <div id="slider-1" class="mx-auto">
-                    <div class="bg-cover bg-center h-auto leading-6 text-orange-900 py-24 px-10 object-fill" style="background-image: url(https://cdn.pixabay.com/photo/2016/08/07/18/22/back-to-school-1576791_960_720.jpg)">
+                    <div class="bg-cover bg-top h-auto leading-6 text-red-900 py-24 px-10 object-fill" style="background-image: url(https://cdn.pixabay.com/photo/2018/03/06/17/40/paper-3204064_960_720.jpg)">
                         <div class="md:w-1/2">
-                        <p class="text-3xl text-black font-extrabold">Romapel</p>
-                        <p class="text-2xl text-black font-bold mb-10 leading-none">Novidades para usar em seus Letterings e Ilustrações</p>
+                        <p class="text-3xl text-black text-center font-extrabold">Romapel</p><br>
+                        <p class="text-3xl text-black text-right font-bold md-10 leading-none">'Pintando o 7 com Romapel'</p>
 
                         </div>
                     </div> <!-- container -->
@@ -27,20 +27,24 @@
                 </div>
 
                 <div id="slider-2" class=" mx-auto">
-                    <div class="bg-cover bg-top  h-auto text-black py-24 px-10 object-fill" style="background-image: url(https://cdn.pixabay.com/photo/2016/09/26/09/59/school-supplies-1695564_960_720.jpg)">
-                        <p class="font-extrabold text-center text-sm uppercase">"Pintando o 7 com Romapel"</p>
-                        <p class="text-3xl text-black text-center font-extrabold">Romapel - A sua Papelaria</p>
-                        <p class="text-2xl text-black text-center mb-10 leading-none">Novidades para usar em seus Letterings e Ilustrações</p>
+                    <div class="bg-cover bg-top h-auto text-black py-24 px-10 object-fill" style="background-image: url(https://cdn.pixabay.com/photo/2016/09/26/09/59/school-supplies-1695564_960_720.jpg)">
+                        <p class="text-3xl text-black text-justify text-sm uppercase font-bold ">"Lançamentos e Novidades"</p>
+                        <p class="text-2xl text-black text-justify font-bold mb-10 leading-none"> 'Romapel - A sua Papelaria'</p>
+                        <br>
                         <a href="http:/http://localhost:3000/Contato//" target="_blank" class="bg-orange-800 py-4 px-8 text-center text-white font-extrabold uppercase text-xs text-center rounded hover:bg-gray-200 hover:text-gray-800">Contato</a>
                     </div> <!-- container -->
                     <br>
                 </div>
             </div>
+            <br>
+
             <div  class="flex justify-between w-12 mx-auto ">
                 <button id="sButton1" onclick="sliderButton1()" class="bg-pink-400 rounded-full w-4 pb-2 " ></button>
-                <button id="sButton2" onclick="sliderButton2() " class="bg-pink-400 rounded-full w-4 p-2"></button>
+                <button id="sButton2" onclick="sliderButton2() " class="bg-pink-800 rounded-full w-4 p-2"></button>
             </div>
+            <br>
         </div>
+        <br>
 
         <div class="container mx-auto">
             <h1 class="text-center text-red-700 py-6 text-5xl ">
@@ -59,31 +63,31 @@
         </div>
 
 
-        <div class="grid grid-cols-4 p-8">
+         <div class="grid md:grid-cols-4 lg:grid-cols-6 sm:grid-cols-3 p-8">
             <div class="col-span-1 py-6 m-2">
                 <div class="relative">
                   <a href="http://localhost:3000/" target="_blank">
-                  <img src="~/assets/img/cat/06-pintura.jpg" alt="" class="rounded-3xl relative" >
+                  <img src="~/assets/img/cat/aquarela.webp" alt="" class="rounded-3xl relative" >
                   <p class="mb-3 p-4 w-full shadow-outline text-xl tracking-wide text-base text-shadow text-teal-800 font-bold">
-                  Aquarela Profissional<br>Preço  R$ 25,00</p></a>
+                  Aquarela <br>Preço  R$ 25,00</p></a>
                 </div>
                 </div>
 
             <div class="col-span-1 py-6 m-2">
                   <div class="relative">
                   <a href="http://localhost:3000/" target="_blank">
-                  <img src="~/assets/img/cat/kitpinceis.png" alt="" class="rounded-3xl relative">
+                  <img src="~/assets/img/cat/pinceis.jpg" alt="" class="rounded-3xl relative">
                   <p class="mb-3 p-4 w-full shadow-outline text-xl tracking-wide text-base text-shadow text-teal-800 font-bold">
-                  Pincéis Profissionais<br>Preço  R$ 10,00</p></a>
+                  Pincéis <br>Preço  R$ 10,00</p></a>
                 </div>
                 </div>
 
             <div class="col-span-1 py-6 m-2">
                   <div class="relative">
                   <a href="http://localhost:3000/" target="_blank">
-                  <img src="~/assets/img/cat/crayons.jpg" alt="" class="rounded-3xl relative">
+                  <img src="~/assets/img/cat/gizpastelecopentel.jpg" alt="" class="rounded-3xl relative">
                   <p class="mb-3 p-4 w-full shadow-outline text-xl tracking-wide text-base text-shadow text-teal-800 font-bold">
-                  Crayons/Giz Pastel<br>Preço  R$ 26,00</p></a>
+                  Giz Pastel Eco<br>Preço  R$ 26,00</p></a>
                 </div>
                 </div>
 
@@ -99,7 +103,7 @@
             <div class="col-span-1 py-6 m-2">
                   <div class="relative">
                   <a href="http://localhost:3000/" target="_blank">
-                  <img src="~/assets/img/cat/brushes03.jpg" alt="" class="rounded-3xl relative">
+                  <img src="~/assets/img/cat/brushes09.jpg" alt="" class="rounded-3xl relative">
                   <p class="mb-3 p-4 w-full shadow-outline text-xl tracking-wide text-base text-shadow text-teal-800 font-bold">
                   Brushes CIS<br>Preço  R$ 36,00</p></a>
                 </div>
@@ -108,7 +112,7 @@
             <div class="col-span-1 py-6 m-2">
                   <div class="relative">
                   <a href="http://localhost:3000/" target="_blank">
-                  <img src="~/assets/img/cat/kitstabilo.jpg" alt="" class="rounded-3xl relative">
+                  <img src="~/assets/img/cat/pastel-stabilo.jpg" alt="" class="rounded-3xl relative">
                   <p class="mb-3 p-4 w-full shadow-outline text-xl tracking-wide text-base text-shadow text-teal-800 font-bold">
                   Kit Marca Texto<br>Preço  R$ 24,00</p></a>
                 </div>
@@ -117,7 +121,7 @@
             <div class="col-span-1 py-6 m-2">
                   <div class="relative">
                   <a href="http://localhost:3000/" target="_blank">
-                  <img src="~/assets/img/cat/aquarelavel.jpg" alt="" class="rounded-3x1 relative">
+                  <img src="~/assets/img/cat/pencil3.jpg" alt="" class="rounded-3x1 relative">
                   <p class="mb-3 p-4 w-full shadow-outline text-xl tracking-wide text-base text-shadow text-teal-800 font-bold">
                   Lápis Colorido Aquarelável<br>Preço  R$ 28,00</p></a>
                 </div>
@@ -126,9 +130,9 @@
             <div class="col-span-1 py-6 m-2">
                   <div class="relative">
                   <a href="http://localhost:3000/" target="_blank">
-                  <img src="~/assets/img/cat/faber2.jpg" alt="" class="rounded-3x1 relative">
+                  <img src="~/assets/img/cat/maletaorganizadora.jpg" alt="" class="rounded-3x1 relative">
                   <p class="mb-3 p-4 w-full shadow-outline text-xl tracking-wide text-base text-shadow text-teal-800 font-bold">
-                  Lápis Colorido<br>Preço  R$ 28,00</p></a>
+                  Maleta<br>Preço  R$ 28,00</p></a>
                 </div>
                 </div>
 
@@ -144,9 +148,9 @@
                 <div class="col-span-1 py-6 m-2">
                   <div class="relative">
                   <a href="http://localhost:3000/" target="_blank">
-                  <img src="~/assets/img/cat/lettering.jpg" alt="" class="rounded-3xl relative">
+                  <img src="~/assets/img/cat/lettering.png" alt="" class="rounded-3xl relative">
                   <p class="mb-3 p-4 w-full shadow-outline text-xl tracking-wide text-base text-shadow text-teal-800 font-bold">
-                  Caderno de Lettering<br>Preço  R$ 27,50</p></a>
+                   Book Lettering<br>Preço  R$ 27,50</p></a>
                 </div>
                 </div>
 
@@ -155,52 +159,160 @@
                   <a href="http://localhost:3000/" target="_blank">
                   <img src="~/assets/img/cat/caligrafia.jpg" alt="" class="rounded-3xl relative">
                   <p class="mb-3 p-4 w-full shadow-outline text-xl tracking-wide text-base text-shadow text-teal-800 font-bold">
-                  Livro Caligrafia Criativa<br>Preço  R$ 31,00</p></a>
+                  Book Caligrafia<br>Preço  R$ 31,00</p></a>
                 </div>
                 </div>
 
                 <div class="col-span-1 py-6 m-2">
                   <div class="relative">
                   <a href="http://localhost:3000/" target="_blank">
-                  <img src="~/assets/img/cat/kitstabilo.jpg" alt="" class="rounded-3xl relative">
+                  <img src="~/assets/img/cat/limpatipo.jpg" alt="" class="rounded-3xl relative">
                   <p class="mb-3 p-4 w-full shadow-outline text-xl tracking-wide text-base text-shadow text-teal-800 font-bold">
-                  Kit Marca Texto<br>Preço  R$ 18,00</p></a>
+                  Limpa-tipo<br>Preço  R$ 4,00</p></a>
                 </div>
                 </div>
 
                  <div class="col-span-1 py-6 m-2">
                   <div class="relative">
                   <a href="http://localhost:3000/" target="_blank">
-                  <img src="~/assets/img/cat/eraser.jpg" alt="" class="rounded-3xl relative">
+                  <img src="~/assets/img/cat/borrachas.jpg" alt="" class="rounded-3xl relative">
                   <p class="mb-3 p-4 w-full shadow-outline text-xl tracking-wide text-base text-shadow text-teal-800 font-bold">
-                  Borrachas<br>Preço  R$ 40,00</p></a>
+                  Borrachas cores sortidas<br>Preço  R$ 40,00</p></a>
                 </div>
                 </div>
 
                 <div class="col-span-1 py-6 m-2">
                   <div class="relative">
                   <a href="http://localhost:3000/" target="_blank">
-                  <img src="~/assets/img/cat/kitpintura.jpg" alt="" class="rounded-3xl relative">
+                  <img src="~/assets/img/cat/kitcolor1.webp" alt="" class="rounded-3xl relative">
                   <p class="mb-3 p-4 w-full shadow-outline text-xl tracking-wide text-base text-shadow text-teal-800 font-bold">
-                  Kit de Pintura<br>Preço  R$ 27,50</p></a>
+                  Kit desenho e pintura<br>Preço  R$ 27,50</p></a>
                 </div>
                 </div>
 
                 <div class="col-span-1 py-6 m-2">
                   <div class="relative">
                   <a href="http://localhost:3000/" target="_blank">
-                  <img src="~/assets/img/cat/sanfonada.jpg" alt="" class="rounded-3xl relative">
+                  <img src="~/assets/img/cat/blackpencil.jpg" alt="" class="rounded-3xl relative">
                   <p class="mb-3 p-4 w-full shadow-outline text-xl tracking-wide text-base text-shadow text-teal-800 font-bold">
-                 Pasta Sanfonada<br>Preço  R$ 31,00</p></a>
+              Boxlápis grafite preta.<br>Preço  R$ 31,00</p></a>
                 </div>
                 </div>
 
                 <div class="col-span-1 py-6 m-2">
                   <div class="relative">
                   <a href="http://localhost:3000/" target="_blank">
-                  <img src="~/assets/img/cat/metalic.jpg" alt="" class="rounded-3xl relative">
+                  <img src="~/assets/img/cat/nanquim.jpg" alt="" class="rounded-3xl relative">
                   <p class="mb-3 p-4 w-full shadow-outline text-xl tracking-wide text-base text-shadow text-teal-800 font-bold">
-                  Lápis Colorido Metalizado<br>Preço  R$ 18,00</p></a>
+               Canetas nanquim.<br>Preço  R$ 18,00</p></a>
+                </div>
+                </div>
+
+                <div class="col-span-1 py-6 m-2">
+                  <div class="relative">
+                  <a href="http://localhost:3000/" target="_blank">
+                  <img src="~/assets/img/cat/kitcolor3.webp" alt="" class="rounded-3xl relative">
+                  <p class="mb-3 p-4 w-full shadow-outline text-xl tracking-wide text-base text-shadow text-teal-800 font-bold">
+                  Kit lápis Color variado<br>Preço  R$ 18,00</p></a>
+                </div>
+                </div>
+
+<div class="col-span-1 py-6 m-2">
+                  <div class="relative">
+                  <a href="http://localhost:3000/" target="_blank">
+                  <img src="~/assets/img/cat/lapiseira01.webp" alt="" class="rounded-3xl relative">
+                  <p class="mb-3 p-4 w-full shadow-outline text-xl tracking-wide text-base text-shadow text-teal-800 font-bold">
+                  Lapiseira e porta grafite<br>Preço  R$ 18,00</p></a>
+                </div>
+                </div>
+
+<div class="col-span-1 py-6 m-2">
+                  <div class="relative">
+                  <a href="http://localhost:3000/" target="_blank">
+                  <img src="~/assets/img/cat/lapiseira02.webp" alt="" class="rounded-3xl relative">
+                  <p class="mb-3 p-4 w-full shadow-outline text-xl tracking-wide text-base text-shadow text-teal-800 font-bold">
+                  Lapiseira<br>Preço  R$ 18,00</p></a>
+                </div>
+                </div>
+
+<div class="col-span-1 py-6 m-2">
+                  <div class="relative">
+                  <a href="http://localhost:3000/" target="_blank">
+                  <img src="~/assets/img/cat/lapiseira03.jpg" alt="" class="rounded-3xl relative">
+                  <p class="mb-3 p-4 w-full shadow-outline text-xl tracking-wide text-base text-shadow text-teal-800 font-bold">
+                  Lapiseira<br>Preço  R$ 18,00</p></a>
+                </div>
+                </div>
+
+                <div class="col-span-1 py-6 m-2">
+                  <div class="relative">
+                  <a href="http://localhost:3000/" target="_blank">
+                  <img src="~/assets/img/cat/lapiseira04.jpg" alt="" class="rounded-3xl relative">
+                  <p class="mb-3 p-4 w-full shadow-outline text-xl tracking-wide text-base text-shadow text-teal-800 font-bold">
+                 Lapiseira<br>Preço  R$ 18,00</p></a>
+                </div>
+                </div>
+
+                <div class="col-span-1 py-6 m-2">
+                  <div class="relative">
+                  <a href="http://localhost:3000/" target="_blank">
+                  <img src="~/assets/img/cat/oilpastel.png" alt="" class="rounded-3xl relative">
+                  <p class="mb-3 p-4 w-full shadow-outline text-xl tracking-wide text-base text-shadow text-teal-800 font-bold">
+                  Pastel/Oleoso<br>Preço  R$ 18,00</p></a>
+                </div>
+                </div>
+
+                <div class="col-span-1 py-6 m-2">
+                  <div class="relative">
+                  <a href="http://localhost:3000/" target="_blank">
+                  <img src="~/assets/img/cat/aquarelavel61cores.jpg" alt="" class="rounded-3xl relative">
+                  <p class="mb-3 p-4 w-full shadow-outline text-xl tracking-wide text-base text-shadow text-teal-800 font-bold">
+                  Lápis Colorido <br>Preço  R$ 18,00</p></a>
+                </div>
+                </div>
+
+                <div class="col-span-1 py-6 m-2">
+                  <div class="relative">
+                  <a href="http://localhost:3000/" target="_blank">
+                  <img src="~/assets/img/cat/aquarelavel.jpg" alt="" class="rounded-3xl relative">
+                  <p class="mb-3 p-4 w-full shadow-outline text-xl tracking-wide text-base text-shadow text-teal-800 font-bold">
+                  Lápis Colorido <br>Preço  R$ 18,00</p></a>
+                </div>
+                </div>
+
+                <div class="col-span-1 py-6 m-2">
+                  <div class="relative">
+                  <a href="http://localhost:3000/" target="_blank">
+                  <img src="~/assets/img/cat/brushes09.jpg" alt="" class="rounded-3xl relative">
+                  <p class="mb-3 p-4 w-full shadow-outline text-xl tracking-wide text-base text-shadow text-teal-800 font-bold">
+                  Brushes tons<br>Preço  R$ 18,00</p></a>
+                </div>
+                </div>
+
+                <div class="col-span-1 py-6 m-2">
+                  <div class="relative">
+                  <a href="http://localhost:3000/" target="_blank">
+                  <img src="~/assets/img/cat/brushcis.jpg" alt="" class="rounded-3xl relative">
+                  <p class="mb-3 p-4 w-full shadow-outline text-xl tracking-wide text-base text-shadow text-teal-800 font-bold">
+                  Brushes CIS<br>Preço  R$ 18,00</p></a>
+                </div>
+                </div>
+
+<div class="col-span-1 py-6 m-2">
+                  <div class="relative">
+                  <a href="http://localhost:3000/" target="_blank">
+                  <img src="~/assets/img/cat/colorbic.jpg" alt="" class="rounded-3xl relative">
+                  <p class="mb-3 p-4 w-full shadow-outline text-xl tracking-wide text-base text-shadow text-teal-800 font-bold">
+                 Kit Escolar BIC<br>Preço  R$ 18,00</p></a>
+                </div>
+                </div>
+
+                <div class="col-span-1 py-6 m-2">
+                  <div class="relative">
+                  <a href="http://localhost:3000/" target="_blank">
+                  <img src="~/assets/img/cat/colorpen2.jpg" alt="" class="rounded-3xl relative">
+                  <p class="mb-3 p-4 w-full shadow-outline text-xl tracking-wide text-base text-shadow text-teal-800 font-bold">
+                  Canetas color<br>Preço  R$ 18,00</p></a>
                 </div>
                 </div>
 
@@ -208,7 +320,7 @@
                 </main>
 
 <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
-  <script>
+<script>
     var cont=0;
 function loopSlider(){
   var xx= setInterval(function(){
@@ -254,7 +366,7 @@ function sliderButton1(){
     $("#slider-1").delay(400).fadeIn(400);
     $("#sButton2").removeClass("bg-purple-800");
     $("#sButton1").addClass("bg-purple-800");
-    reinitLoop(40);
+    reinitLoop(4000);
     cont=0
 
     }
@@ -264,7 +376,7 @@ function sliderButton1(){
     $("#slider-2").delay(400).fadeIn(400);
     $("#sButton1").removeClass("bg-purple-800");
     $("#sButton2").addClass("bg-purple-800");
-    reinitLoop(40);
+    reinitLoop(4000);
     cont=1
 
     }
@@ -275,18 +387,11 @@ function sliderButton1(){
 
 
         loopSlider();
-
-
-
-
-
-
-    });
-
-
+});
   </script>
 </body>
 </html>
 
 </section>
 </template>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
 <section id="main">
-  <Carousel />
+  <Carousel/>
   <Cards />
 
 </section>
