bf9d82219d745fb4e11a15bebc2e2583f37cb7df
cristianebeel <cristianebeel@live.com>
2021-05-16 21:31:29 -0300

    Adicionando Sessões de Informações - Footer

---
+++ components/Informacoes.vue
@@ -0,0 +1,19 @@
+<template>
+    <p>
+        <span v-for="(item, index) of items" :key="index" class="block">
+            {{ item.text }}
+        </span>
+    </p>
+</template>
+
+<script>
+export default {
+    name: 'Informacoes',
+    props: {
+        items: {
+            type: Array,
+            default: () => []
+        }
+    }
+}
+</script>
