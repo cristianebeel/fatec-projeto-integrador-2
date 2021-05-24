8702d314a7650d2a617e271b348eba7402312003
cristianebeel <cristianebeel@live.com>
2021-05-24 07:49:23 -0300

    Adicionando página Escolar

---
+++ components/Card-Produto.vue
@@ -0,0 +1,29 @@
+<template>
+    <article class="w-44">
+        <img :src="imagem" :alt="titulo" class="w-full">
+        <div class="pt-3 pb-7 text-center">
+            <p class="text-gray-80 font-bold text-lg">{{ titulo }}</p>
+            <p class="text-gray-800 text-lg">{{ preco }}</p>
+        </div>
+    </article>
+</template>
+
+<script>
+export default {
+    name:'Card-Produto',
+    props: {
+        titulo: {
+            type: String,
+            default: ''
+        },
+        preco: {
+            type: String,
+            default: ''
+        },
+        imagem: {
+            type: String,
+            default: ''
+        }
+    }
+}
+</script>
---
+++ pages/escolar.vue
@@ -0,0 +1,25 @@
+<template v-if="produtos.categoria.includes('Escolar')">
+    <div id="page" class="grid md:grid-cols-3 lg:grid-cols-5 sm:grid-cols-3 p-8">
+        <Card-Produto
+            v-for="item of produtos"
+            :key="item.titulo"
+            :titulo="item.titulo"
+            :preco="item.preco"
+            :imagem="item.imagem"
+        />
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+    nome: 'Produtos',
+
+    computed:{
+        ...mapState({
+            produtos: state => state.produtos.produtos
+        })
+    }
+}
+</script>
