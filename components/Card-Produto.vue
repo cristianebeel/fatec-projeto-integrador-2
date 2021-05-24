0f556685988609ef4908e8507127545a3405e6b7
cristianebeel <cristianebeel@live.com>
2021-05-24 20:50:28 -0300

    Criando componente de card para produtos

--- components/Card-Produto.vue
+++ components/Card-Produto.vue
@@ -1,12 +1,17 @@
 <template>
-    <article class="w-44">
-        <img :src="imagem" :alt="titulo" class="w-full">
-        <div class="pt-3 pb-7 text-center">
-            <p class="text-gray-80 font-bold text-lg">{{ titulo }}</p>
-            <p class="text-gray-800 text-lg">{{ preco }}</p>
-        </div>
+    <article class="h-56 w-32 bg-white rounded-lg relative text-center py-2 font-bold">
+        <img :src="imagem" :alt="titulo" class="object-contain h-32 px-2">
+            <p class="title text-gray-800 px-1.5 py-1 break-words">{{ titulo }}</p>
+            <p class="bg-indigo-600 text-white absolute bottom-0 w-full text-sm py-1.5">R${{ preco }}</p>
     </article>
 </template>
+
+<style scoped>
+.title{
+    font-size: .8rem;
+    line-height: 1rem;
+}
+</style>
 
 <script>
 export default {
