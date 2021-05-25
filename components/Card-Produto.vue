734e90c48c4f2d9f1f26970d965cc06e8e859573
cristianebeel <cristianebeel@live.com>
2021-05-25 01:13:02 -0300

    Ajustando responsividade

--- components/Card-Produto.vue
+++ components/Card-Produto.vue
@@ -1,15 +1,21 @@
 <template>
-    <article class="h-56 w-32 bg-white rounded-lg relative text-center py-2 font-bold">
-        <img :src="imagem" :alt="titulo" class="object-contain h-32 px-2">
-            <p class="title text-gray-800 px-1.5 py-1 break-words">{{ titulo }}</p>
-            <p class="bg-indigo-600 text-white absolute bottom-0 w-full text-sm py-1.5">R${{ preco }}</p>
+    <article class="h-56 w-32 bg-white rounded-lg relative text-center py-2 font-bold
+        sm:h-64 sm:w-44 md:h-72 md:w-56 lg:h-80 xl:h-80 2xl:h-96 2xl:w-64">
+        <img :src="imagem" :alt="titulo" class="object-contain h-32 w-full px-2
+            sm:h-36 md:h-40 lg:h-48">
+        <p class="title text-gray-800 px-1.5 py-1 leading-tight
+            sm:px-2 sm:py-3 sm:text-sm md:text-base md:py-5
+            lg:text-lg lg:py-2 xl:text-xl xl:py-4">{{ titulo }}</p>
+        <p class="bg-indigo-600 text-white absolute bottom-0 w-full text-sm py-1.5 rounded-b-lg
+            hover:bg-green-700 md:py-2 md:text-base lg:text-lg xl:text-xl">R${{ preco }}</p>
     </article>
 </template>
 
 <style scoped>
-.title{
-    font-size: .8rem;
-    line-height: 1rem;
+@media(max-width: 320px){
+    .title{
+        font-size: .75rem;
+    }
 }
 </style>
 
--- pages/escolar.vue
+++ pages/escolar.vue
@@ -1,5 +1,6 @@
 <template>
-  <div id="page" class="grid grid-cols-2 gap-y-2 px-4 mx-auto sm:grid-cols-3 lg:grid-cols-5">
+  <div class="grid grid-cols-2 gap-y-2 px-4 py-8 justify-items-center cursor-pointer
+    sm:grid-cols-3 sm:gap-y-5 lg:grid-cols-4 xl:grid-cols-5 xl:px-14 xl:gap-y-12">
     <div v-for="item of produtos" :key="item.titulo">
       <Card-Produto
         v-if="item.categoria.includes('Escolar')"
