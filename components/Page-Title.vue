883fbeef09ead15bf6e4abb8d93813b79187ed65
cristianebeel <cristianebeel@live.com>
2021-05-25 20:30:23 -0300

    Criando componente para título de página

---
+++ components/Page-Title.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1 class="text-xl font-bold text-center mt-9 mb-6 md:text-2xl lg:text-3xl xl:text-4xl" :class="cor">
+        <slot />
+    </h1>
+</template>
+
+<script>
+export default {
+    name: 'Page-Title',
+    props:{
+        cor:{
+            type: String,
+            default: 'text-gray-700'
+        }
+    }
+}
+</script>
