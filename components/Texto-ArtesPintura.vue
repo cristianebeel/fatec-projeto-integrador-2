7dc2e0254b01fa7fc1a68a3613b31e818f259f62
cristianebeel <cristianebeel@live.com>
2021-05-26 02:46:54 -0300

    Texto Artes e Pintura

---
+++ components/Texto-ArtesPintura.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="w-3/4 mx-auto">
+        <h2 class=" font-bold text-center text-green-800 mt-12 mb-6 text-3xl">
+            Novidades para usar em seus Letterings e Ilustrações
+        </h2>
+        <div class="flex flex-row content-center space-x-6">
+            <img src="~/assets/img/gift.svg" aria-hidden="true" class="h-24">
+            <div class="flex-col space-y-3">
+                <p class="text-green-700 font-bold leading-normal">
+                    Lettering é “a arte de desenhar letras que encanta com delicadeza”, expressando seus sentimentos com criatividade e identidade visual singular: uma combinação específica de letras trabalhadas, para uma única utilização e finalidade.
+                </p>
+                <p class="text-green-700 font-bold leading-normal">
+                    Ela começou a ser utilizada por designers para criação de logotipos e composições all-type, mas nos últimos anos vem trazendo paixão aos amantes de papelaria.
+                </p>
+                <p class="text-green-700 font-bold leading-normal">
+                    O lettering é desenhado, na maioria das vezes, desenhado à mão, com lápis, canetas, pinceis e pode ser trabalhado digitalmente.
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
