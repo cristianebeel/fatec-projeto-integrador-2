ceaf4f2df95ea090742af00f6050c494a9860ef3
Maria Silvia de Carvalho Bueno <78733533+MSILVIACB@users.noreply.github.com>
2021-05-26 20:53:08 -0300

    Create Sobre.vue

---
+++ components/Sobre.vue
@@ -0,0 +1,24 @@
+<template>
+<section id="page" class="my-10">
+
+<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-3xl">
+  <div class="md:flex ">
+    <div class="md:flex-shrink-0">
+      <img class="h-64 w-full object-cover rounded-lg md:h-full md:w-72" src="~assets/img/cat/logoroma.png" alt="Man looking at item at a store">
+    </div>
+    <div class="p-8">
+      <div class="uppercase tracking-wide text-sm text-3xl text-pink-800 font-extrabold">Romapel</div>
+      <a href="#" class="block mt-1 text-sm text-3xl leading-tight font-bold text-pink-700 hover:underline">Papelaria Roma - Romapel</a>
+      <p class="mt-2 font-semibold text-black"> A Papelaria Roma é uma empresa sediada na cidade de Monte Alto/SP.
+         Na Papelaria Roma ou Romapel você encontra os mais variados itens de Material Escolar; Escritório;
+         Mochilas; Pastas e Malas; Xerox Preto e Colorido; Impressão, Currículo e Declaração; Encadernação e
+          Plastificação; Papéis Especiais; Tintas e Pincéis; Itens artesanais; Lembrancinhas e Brindes
+          Personalizados; E muito mais...A Papelaria Roma é a sua Papelaria Online, com Material Escolar,
+           Pintura e Escritório com bons preços e entrega para todo Brasil. Tudo o que você precisa para
+            seu Escritório, Consultório ou Home Office em um só lugar.</p>
+  </div>
+</div>
+
+</div>
+</section>
+</template>
