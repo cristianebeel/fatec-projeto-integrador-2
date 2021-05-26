ac2794e4f3e28f76c22d51f03b7a0a73041d8456
cristianebeel <cristianebeel@live.com>
2021-05-26 01:16:51 -0300

    Colorindo título e subtítulo de sessões

--- components/Section-Subtitle.vue
+++ components/Section-Subtitle.vue
@@ -1,5 +1,5 @@
 <template>
-    <h3 class="text-md font-bold text-center mt-3 mb-1 md:text-lg lg:text-xl xl:text-2xl">
+    <h3 class="text-md font-bold text-center text-gray-600 mt-3 mb-1 md:text-lg lg:text-xl xl:text-2xl">
         <slot />
     </h3>
 </template>
--- components/Section-Title.vue
+++ components/Section-Title.vue
@@ -1,5 +1,5 @@
 <template>
-    <h2 class="text-lg font-bold text-center mt-3 mb-4 md:text-xl lg:text-2xl xl:text-3xl">
+    <h2 class="text-lg font-bold text-center text-gray-700 tracking-wide mt-3 mb-4 md:text-xl lg:text-2xl xl:text-3xl">
         <slot />
     </h2>
 </template>
