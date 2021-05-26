d7c819d83c4977244d5e4897f47f5daa5dfe1272
cristianebeel <cristianebeel@live.com>
2021-05-26 01:11:07 -0300

    Arrumando componente de título das páginas

--- components/Page-Title.vue
+++ components/Page-Title.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1 class="text-xl font-bold text-center mt-9 mb-6 md:text-2xl lg:text-3xl xl:text-4xl" :class="cor">
+    <h1 class="text-xl font-bold text-center mt-9 mb-6 tracking-wider pt-2 pb-1 w-full md:text-2xl lg:text-3xl xl:text-4xl" :class="cor">
         <slot />
     </h1>
 </template>
