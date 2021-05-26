dc0bfa49046e141c4c821682709a7040bea91ff9
cristianebeel <cristianebeel@live.com>
2021-05-26 01:10:44 -0300

    Adicionando banners páginas internas

---
+++ components/Banner-Escritorio.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="w-full h-32 bg-top sm:h-44 md:bg-top lg:h-80 xl:h-96"></div>
+</template>
+
+<style scoped>
+div{
+    background-image: url(~/assets/img/banners/banner-11.jpg);
+    background-size: cover;
+    background-repeat: no-repeat;
+}
+</style>
