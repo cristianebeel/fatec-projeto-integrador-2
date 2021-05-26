34e541d4a0a1fdfd14f206475213cda5aef528e1
cristianebeel <cristianebeel@live.com>
2021-05-26 04:52:44 -0300

    Adicionando validação

--- pages/contato.vue
+++ pages/contato.vue
@@ -1,29 +1,20 @@
 <template>
     <section id="main" class="mx-10 mb-10">
-        <h1 class="my-3 text-3xl font-semibold text-gray-800 dark:text-gray-200">Contato</h1>
-        <p class="text-gray-900 dark:text-gray-400 text-base">Fale conosco através do formulário abaixo. Retornaremos o mais rápido possível.</p>
-        <p id="result" class="text-red-600 mx-3 mt-2 text-sm">{{ errormsg }}</p>
-        <form class="w-9/12 mt-7">
+        <Page-Title>Contato</Page-Title>
+        <p class="text-gray-900 dark:text-gray-400 text-base text-center">Fale conosco através do formulário abaixo. Retornaremos o mais rápido possível.</p>
+        <form @submit.prevent="send" class="mx-auto w-9/12 mt-7">
             <label for="name">Nome</label>
-            <input type="text" name="name" id="name" placeholder="Seu nome" required />
+            <input type="text" v-model="name" name="name" id="name" placeholder="Seu nome" required />
 
             <label for="email">E-mail</label>
-            <input type="email" name="email" id="email" placeholder="Seu e-mail" required />
-
-            <!-- <label for="subject">Assunto</label>
-            <select id="subject" name="subject" required>
-                <option value="a" selected disabled>Selecione uma opção</option>
-                <option>Sugestão</option>
-                <option>Dúvida</option>
-                <option>Reclamação</option>
-            </select> -->
+            <input type="email" v-model="email" name="email" id="email" placeholder="Seu e-mail" required />
 
             <label for="message">Sua mensagem</label>
-            <textarea rows="5" name="message" id="message" placeholder="Escreva sua mensagem" required></textarea>
+            <textarea rows="5" v-model="message" name="message" id="message" placeholder="Escreva sua mensagem" required></textarea>
 
             <div class="flex justify-end gap-4 mx-1">
-                <button type="reset" v-on:click="clear" class="py-2 px-6 text-white bg-red-500 rounded-md hover:bg-red-600 focus:bg-red-700 focus:outline-none">Limpar</button>
-                <button type="submit" v-on:click="send" class="py-2 px-10 text-white bg-green-600 rounded-md hover:bg-green-500 focus:bg-green-700 focus:outline-none">Enviar</button>
+                <button type="reset" class="py-2 px-6 text-white bg-red-500 rounded-md hover:bg-red-600 focus:bg-red-700 focus:outline-none">Limpar</button>
+                <button type="submit" class="py-2 px-10 text-white bg-green-600 rounded-md hover:bg-green-500 focus:bg-green-700 focus:outline-none">Enviar</button>
             </div>
         </form>
     </section>
@@ -70,18 +61,21 @@ select {
 <script>
 export default {
     name: 'contato',
-    data () {
-        return {
-            errormsg: ''
+
+    data(){
+        return{
+            name: "",
+            email: "",
+            message: ""
         }
     },
+
     methods: {
         send () {
-            if(!this.name || !this.email || !this.textarea)
-                this.errormsg = '*Preencha todos os campos!'
-        },
-        clear () {
-            this.errormsg=''
+            this.name = ""
+            this.email = ""
+            this.message = ""
+            alert('Sua mensagem foi enviada com sucesso!')
         }
     }
 }
