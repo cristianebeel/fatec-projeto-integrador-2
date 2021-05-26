887a6480593435acefbceb3bcb40fc61be9db06d
cristianebeel <cristianebeel@live.com>
2021-05-26 04:38:33 -0300

    Adicionando chat real-time

--- components/Header.vue
+++ components/Header.vue
@@ -4,7 +4,7 @@
             <div class="md:hidden"><img src="~/assets/img/menu-hamburger.svg" alt="Menu" class="h-5"></div>
             <nuxt-link to="/"><Logo /></nuxt-link>
             <div class="flex flex-row">
-                <nuxt-link to="/"><img src="~/assets/img/contact.svg" alt="Fale conosco" class="h-5"></nuxt-link>
+                <nuxt-link to="/chat"><img src="~/assets/img/contact.svg" alt="Fale conosco" class="h-5"></nuxt-link>
                 <nuxt-link to="/"><img src="~/assets/img/heart.svg" alt="Minha lista" class="h-5"></nuxt-link>
                 <nuxt-link to="/"><img src="~/assets/img/cart.svg" alt="Minha sacola" class="h-5"></nuxt-link>
             </div>
