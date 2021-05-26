bdb9075ad7ec6c5884afb7df1c6033a2ab75785c
cristianebeel <cristianebeel@live.com>
2021-05-26 00:53:14 -0300

    Adicionando banner e título

--- pages/escolar.vue
+++ pages/escolar.vue
@@ -1,14 +1,18 @@
 <template>
-  <div class="grid grid-cols-2 gap-y-2 px-4 py-8 justify-items-center cursor-pointer
-    sm:grid-cols-3 sm:gap-y-5 lg:grid-cols-4 xl:grid-cols-5 xl:px-14 xl:gap-y-12">
-    <div v-for="item of produtos" :key="item.titulo">
-      <Card-Produto
-        v-if="item.categoria.includes('Escolar')"
-        :titulo="item.titulo"
-        :preco="item.preco"
-        :imagem="item.imagem"
-        :categoria="item.categoria"
-      />
+  <div>
+    <Banner-Escolar />
+    <Page-Title cor="text-purple-600">Materias Escolares</Page-Title>
+    <div class="flex flex-row flex-wrap justify-center w-full gap-y-4 gap-x-2 py-4 cursor-pointer
+      sm:grid-cols-3 sm:gap-y-5 lg:grid-cols-4 xl:grid-cols-5 xl:px-14 xl:gap-y-12">
+      <div v-for="item of produtos" :key="item.titulo">
+        <Card-Produto
+          v-if="item.categoria.includes('Escolar')"
+          :titulo="item.titulo"
+          :preco="item.preco"
+          :imagem="item.imagem"
+          :categoria="item.categoria"
+        />
+      </div>
     </div>
   </div>
 </template>
