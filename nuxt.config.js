af8d124652bd024e9fb827aed8903e037e8fe225
cristianebeel <cristianebeel@live.com>
2021-05-26 04:37:37 -0300

    Alterando título na barra do navegador

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,9 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'fatec-projeto-integrador-2',
+    title: 'Romapel Papelaria',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'pt-br'
     },
     meta: [
       { charset: 'utf-8' },
