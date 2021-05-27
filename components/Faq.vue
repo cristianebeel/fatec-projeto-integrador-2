dc0ffd62f0c5fe834d1e4e596a79ddd1177cf247
Maria Silvia de Carvalho Bueno <78733533+MSILVIACB@users.noreply.github.com>
2021-05-27 17:16:05 -0300

    Create Faq.vue
    Componente Faq.

---
+++ components/Faq.vue
@@ -0,0 +1,110 @@
+<template>
+
+<div>
+  <br>
+      <section class="text-blue-900 text-lg">
+        <div class="container bg-gray-200 px-5 py-18 max-w-8xl mx-auto">
+          <div class="text-center font-bold  text-xl mb-20">
+            <h1 class="sm:text-3xl text-2xl font-bold text-center title-font text-gray-900 mb-4">
+              Perguntas mais frequentes
+            </h1>
+            <p class="text-xl text-indigo-900 font-bold leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+              As perguntas mais comuns sobre o que podemos fazer por você.
+            </p>
+          </div>
+
+
+          <div class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+            <div class="w-full lg:w-1/2 px-4 py-2">
+          <details class="mb-4">
+                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                 Consigo rastrear o minha Compra?
+                </summary>
+                <span>
+                  Sim. As informações mais atualizadas sobre sua Compra e a situação de entrega de sua
+                  Compra estão disponíveis em 'Meus pedidos'.
+                  Além disso, você será notificado do andamento do seu pedido pelo e-mail cadastrado.
+                </span>
+          </details>
+
+              <details class="mb-4">
+                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                 Qual a empresa responsável pela entrega da minha Compra?
+                </summary>
+                <span>
+                  As entregas são realizadas exclusivamente pelos Correios.
+                </span>
+             </details>
+
+              <details class="mb-4">
+                <summary class="font-semibold bg-gray-200 rounded-md py-2 px-4">
+                  Posso fazer minha Compra no site e buscar os produtos adquiridos na loja física?
+                </summary>
+
+                <span>
+                  Não. Ao efetuar uma Compra pelo site, você receberá o(s) produto(s) adquirido(s) no endereço informado,
+                   por meio dos mecanismos de entrega disponibilizados pela Romapel no site.
+                </span>
+              </details>
+
+              <details class="mb-4">
+                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                  Posso adicionar outro produto à Compra que eu fiz?
+                </summary>
+
+                <span>
+                 Não, após finalizada, a Compra não poderá ser alterada.
+                 Para adquirir outro produto, você deverá iniciar uma nova Compra.
+                </span>
+              </details>
+            </div>
+            <div class="w-full lg:w-1/2 px-4 py-2">
+              <details class="mb-4">
+                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                  Não recebi todos os itens da minha Compra
+                </summary>
+
+                <span class="px-4 py-2">
+                  Entre em contato com o nossa loja física pelo telefone
+                 (16) 3243-2696 ou pelos outros meios de atendimento disponibilizados.
+                </span>
+              </details>
+              <details class="mb-4">
+                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                  Posso efetuar uma Compra no site com CNPJ?
+                </summary>
+
+                <span class="px-4 py-2">
+                  Neste momento, não estamos trabalhando com esta opção. Apenas pessoas físicas podem se cadastrar no site e efetuar Compras.
+                   A aquisição de produtos por meio de CNPJ pode ser feito através de nossa página no Facebook @papelariaromapel.
+                </span>
+              </details>
+              <details class="mb-4">
+                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                  Como faço para confirmar a disponibilidade de um produto no estoque?
+                </summary>
+
+                <span class="px-4 py-2">
+                  No site, se houver carrinho de compras visível no momento da finalização do
+                  pedido, significa que há estoque do produto.
+                </span>
+               </details>
+
+               <details class="mb-4">
+                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                 Existe um valor mínimo de pedido?
+                </summary>
+                <span>
+                  Não.
+                </span>
+             </details>
+
+
+
+
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+    </template>
