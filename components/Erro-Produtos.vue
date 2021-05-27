08e8c49882d01c76602e9ddfcb5147e50e3122e8
cristianebeel <cristianebeel@live.com>
2021-05-27 16:21:57 -0300

    Adicionando mensagem de página sem produtos

---
+++ components/Erro-Produtos.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="w-full text-center mb-6">
+    <h2 class="text-xl text-gray-600 font-bold lg:text-2xl 2xl:text-3xl">
+      Desculpe
+    </h2>
+    <p class="text-lg text-gray-700 lg:text-xl 2xl:text-2xl">
+      Nenhum produto encontrado
+    </p>
+  </div>
+</template>
