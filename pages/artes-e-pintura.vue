9cd62f6bab67abb234717b27c0a2b6d3b647c689
cristianebeel <cristianebeel@live.com>
2021-05-27 22:58:10 -0300

    Atualizando páginas de produtos

--- pages/artes-e-pintura.vue
+++ pages/artes-e-pintura.vue
@@ -3,18 +3,30 @@
     <Banner-ArtePintura />
     <Texto-ArtesPintura />
     <Page-Title cor="text-yellow-700">{{$t('artAndPainting')}}</Page-Title>
-    <div class="flex flex-row flex-wrap justify-center w-full gap-y-4 gap-x-2 py-4 cursor-pointer
-      sm:grid-cols-3 sm:gap-y-5 lg:grid-cols-4 xl:grid-cols-5 xl:px-14 xl:gap-y-12">
-      <div v-for="item of produtos" :key="item.titulo">
+
+    <div v-if="!produtos.length">
+      <Erro-Produtos />
+    </div>
+
+    <div v-else>
+      <div class="grid grid-flow-row grid-cols-2 gap-y-4 mx-2 justify-items-center
+        sm:grid-cols-3 sm:mx-8 sm:gap-y-6 lg:max-w-screen-lg lg:mx-auto lg:grid-cols-4 lg:gap-x-10
+        xl:mx-12 xl:max-w-screen-xl xl:mx-auto xl:gap-x-20 2xl:gap-x-40">
         <Card-Produto
-          v-if="item.categoria.includes('Arte')"
+          v-for="item of produtosAtuais" :key="item.titulo"
           :titulo="item.titulo"
           :preco="item.preco"
           :imagem="item.imagem"
-          :categoria="item.categoria"
         />
       </div>
+      <div v-if="paginasTotais > 1" class="flex flex-row space-x-2 mt-8 justify-center">
+        <nuxt-link v-for="n in paginasTotais" :key="n" :to="`?page=${n}`"
+          class="bg-red-100 rounded-md p-3 hover:bg-indigo-200">
+          {{ n }}
+        </nuxt-link>
+      </div>
     </div>
+
   </div>
 </template>
 
@@ -22,12 +34,39 @@
 import { mapState } from "vuex";
 
 export default {
-  nome: "Produtos",
+  name: "artes-e-pintura",
+
+  data(){
+    return{
+      paginaAtual: 1
+    }
+  },
 
   computed: {
     ...mapState({
-      produtos: (state) => state.produtos.produtos
+      produtos: (state) => state.produtos.produtos.filter(item => item.categoria === 'Arte')
     }),
+
+    paginasTotais(){
+      return Math.ceil(this.produtos.length / 12)
+    },
+
+    produtosAtuais(){
+      return this.produtos.slice(
+        (this.paginaAtual - 1) * 12,
+        (this.paginaAtual - 1) * 12 + 12
+      )
+    }
   },
+
+  watch: {
+    '$route.query':{
+      immediate: true,
+
+      handler(query){
+        this.paginaAtual = query.page
+      }
+    }
+  }
 };
 </script>
--- pages/categoria/pintura.vue
+++ pages/categoria/pintura.vue
@@ -1,17 +1,30 @@
 <template>
   <div>
     <Categorias-Title>{{$t('painting')}}</Categorias-Title>
-    <div class="flex flex-row flex-wrap justify-center w-full gap-y-4 gap-x-2 py-4 cursor-pointer sm:gap-y-5 xl:px-14 xl:gap-y-12">
-      <div v-for="item of produtos" :key="item.titulo">
+
+    <div v-if="!produtos.length">
+      <Erro-Produtos />
+    </div>
+
+    <div v-else>
+      <div class="grid grid-flow-row grid-cols-2 gap-y-4 mx-2 justify-items-center
+        sm:grid-cols-3 sm:mx-8 sm:gap-y-6 lg:max-w-screen-lg lg:mx-auto lg:grid-cols-4 lg:gap-x-10
+        xl:mx-12 xl:max-w-screen-xl xl:mx-auto xl:gap-x-20 2xl:gap-x-40">
         <Card-Produto
-          v-if="item.subcategoria.includes('Pintura')"
+          v-for="item of produtosAtuais" :key="item.titulo"
           :titulo="item.titulo"
           :preco="item.preco"
           :imagem="item.imagem"
-          :categoria="item.categoria"
         />
       </div>
+      <div v-if="paginasTotais > 1" class="flex flex-row space-x-2 mt-8 justify-center">
+        <nuxt-link v-for="n in paginasTotais" :key="n" :to="`?page=${n}`"
+          class="bg-red-100 rounded-md p-3 hover:bg-indigo-200">
+          {{ n }}
+        </nuxt-link>
+      </div>
     </div>
+
   </div>
 </template>
 
@@ -19,13 +32,39 @@
 import { mapState } from "vuex";
 
 export default {
-  nome: 'caderno',
-  layout: 'categorias',
+  name: "pintura",
+
+  data(){
+    return{
+      paginaAtual: 1
+    }
+  },
 
   computed: {
     ...mapState({
-      produtos: (state) => state.produtos.produtos
+      produtos: (state) => state.produtos.produtos.filter(item => item.subcategoria === 'Pintura')
     }),
+
+    paginasTotais(){
+      return Math.ceil(this.produtos.length / 12)
+    },
+
+    produtosAtuais(){
+      return this.produtos.slice(
+        (this.paginaAtual - 1) * 12,
+        (this.paginaAtual - 1) * 12 + 12
+      )
+    }
   },
+
+  watch: {
+    '$route.query':{
+      immediate: true,
+
+      handler(query){
+        this.paginaAtual = query.page
+      }
+    }
+  }
 };
 </script>
