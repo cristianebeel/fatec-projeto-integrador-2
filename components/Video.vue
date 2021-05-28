554b7b32dd88b2f91266edccf9675877ef3bc62a
cristianebeel <cristianebeel@live.com>
2021-05-28 03:03:16 -0300

    Adicionando responsividade ao vídeo

--- components/Video.vue
+++ components/Video.vue
@@ -1,9 +1,29 @@
 <template>
-<div id="carregando" class="center" align="center">
-<br><br>
-
-<iframe width="640" height="384" src="https://www.youtube.com/embed/EDjdXS4lshU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen ></iframe>
-<br><br>
-
-</div>
+  <section id="video" class="bg-white w-full">
+    <div id="carregando" class="flex justify-center py-6">
+    <div class="video-sobre">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/EDjdXS4lshU" frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+      </iframe>
+    </div>
+  </div>
+  </section>
 </template>
+
+<style scoped>
+@media (max-width:728px) {
+  .video-sobre {
+    position: relative;
+    padding-bottom: 56.25%;
+    padding-top: 25px;
+    height: 0;
+  }
+  .video-sobre iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
