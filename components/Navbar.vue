49c806a596e0495cb84be6e870de4e703e3ca2d6
cristianebeel <cristianebeel@live.com>
2021-05-28 00:34:00 -0300

    Arrumando links e alinhando idiomas

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,20 +1,21 @@
 <template>
-    <section id="navbar" class="navbar shadow-xl">
-        <nav class="text-white text-base">
-            <div class="hidden md:flex flex-row py-3 justify-around items-center">
-                    
-                        <Dropdown />
-                    <div class="space-x-10 lg:space-x-30 xl:space-x-40">
-                        <nuxt-link to="/escolar" class="link">{{$t('school')}}</nuxt-link>
-                        <nuxt-link to="/escritorio" class="link">{{$t('office')}}</nuxt-link>
-                        <nuxt-link to="/artes-e-pintura" class="link">{{$t('artAndPainting')}}</nuxt-link>
-                        <nuxt-link to="/contato" class="link">{{$t('contact')}}</nuxt-link>
-                        <nuxt-link :to="switchLocalePath('en')" class="link">EN</nuxt-link>
-                        <nuxt-link :to="switchLocalePath('pt-br')" class="link">BR</nuxt-link>
-                    </div>
-            </div>
-        </nav>
-    </section>
+  <section id="navbar" class="navbar shadow-xl">
+    <nav class="text-white text-base">
+      <div class="flex flex-row py-3 justify-around items-center">
+        <Dropdown />
+        <div class="hidden md:flex flex-row space-x-10 lg:space-x-30 xl:space-x-40">
+          <nuxt-link to="/escolar?page=1" class="link">{{$t('school')}}</nuxt-link>
+          <nuxt-link to="/escritorio?page=1" class="link">{{$t('office')}}</nuxt-link>
+          <nuxt-link to="/artes-e-pintura?page=1" class="link">{{$t('artAndPainting')}}</nuxt-link>
+          <nuxt-link to="/contato" class="link">{{$t('contact')}}</nuxt-link>
+          <span>
+            <nuxt-link :to="switchLocalePath('en')" class="link">EN</nuxt-link> | 
+            <nuxt-link :to="switchLocalePath('pt-br')" class="link">BR</nuxt-link>
+          </span>
+        </div>
+      </div>
+    </nav>
+  </section>
 </template>
 
 <style scoped>
@@ -27,6 +28,7 @@ nav img{
 }
 
 .link:hover{
-    text-decoration: underline;
+    text-decoration: none;
+    color: indigo;
 }
 </style>
